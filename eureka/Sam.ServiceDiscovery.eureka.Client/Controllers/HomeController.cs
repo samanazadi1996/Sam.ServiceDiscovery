01a64270e9d01242da9a53e355ebd40a7d8bf9ab
Saman Azadi <s.azadi@DESKTOP-VJHOBTS>
2022-11-19 17:34:38 +0330

    add eureka

---
+++ eureka/Sam.ServiceDiscovery.eureka.Client/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Steeltoe.Common.Discovery;
+using Steeltoe.Discovery;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Sam.ServiceDiscovery.eureka.Client.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        DiscoveryHttpClientHandler _handler;
+
+        public HomeController(IDiscoveryClient client)
+        {
+            _handler = new DiscoveryHttpClientHandler(client);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            var list = new List<string>();
+
+            var client = new HttpClient(_handler, false);
+
+            for (int i = 0; i < 20; i++)
+                list.Add(await client.GetStringAsync("http://Sam_ServiceDiscovery_eureka_app/Home"));
+
+            return list;
+        }
+    }
+}
---
+++ eureka/Sam.ServiceDiscovery.eureka.app2/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sam.ServiceDiscovery.eureka.app2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Instance 2";
+        }
+    }
+
+}
